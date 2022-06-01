9da40b73cbdacbdc3af54a620157739842f99a3a
Josenene8 <100051827+Josenene8@users.noreply.github.com>
2022-06-01 15:43:40 -0300

    Update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -75,20 +75,20 @@ class Main extends Sprite
 			gameHeight = Math.ceil(stageHeight / zoom);
 		}
 
-		#if cpp
-		initialState = Caching;
-		game = new FlxGame(gameWidth, gameHeight, initialState, zoom, framerate, framerate, skipSplash, startFullscreen);
-		#else
-		game = new FlxGame(gameWidth, gameHeight, initialState, zoom, framerate, framerate, skipSplash, startFullscreen);
-		#end
-		addChild(game);
+	//	#if cpp
+	//	initialState = Caching;
+	//	game = new FlxGame(gameWidth, gameHeight, initialState, zoom, framerate, framerate, skipSplash, startFullscreen);
+	//	#else
+	//	game = new FlxGame(gameWidth, gameHeight, initialState, zoom, framerate, framerate, skipSplash, startFullscreen);
+	//	#end
+	//	addChild(game);
 		
-		#if !mobile
+	//	#if !mobile
 		fpsCounter = new FPS(10, 3, 0xFFFFFF);
 		addChild(fpsCounter);
 		toggleFPS(FlxG.save.data.fps);
 
-		#end
+	//	#end
 	}
 
 	var game:FlxGame;
